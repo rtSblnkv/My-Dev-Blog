ca894c7a3e4d1db7cd3e18ab61a1ac34889db680
Misha <basckackov.mixail2015@yandex.ru>
2021-03-26 17:28:25 +0400

    Верстка карточки

--- src/components/Card-Blog/Card_blog.js
+++ src/components/Card-Blog/Card_blog.js
@@ -5,8 +5,12 @@ export const Cardblog = () => {
     return(
         <div className="card">
             <div className="card-content">
-                <div className="card-head">Управлене памятью в JS</div>
+                <div className="card-head">Управление памятью в JS</div>
                 <div className="card-text ">Статья об управлении памятью и принципах работы сборщика мусора, а также о том, как избежать самых распространенных видов утечек памяти.</div>
+                <div className="card-info"> 
+                    <div className="card-icons"></div>
+                    <div className="card-autor">Автор: Chupaha777</div>
+                </div>
             </div>
         </div>
     );
