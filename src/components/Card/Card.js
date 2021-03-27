63253bda43b083fc1a30ac3d2c03fc9027dde20c
rtSblnkv <artsab001@gmail.com>
2021-03-27 16:21:05 +0400

    .

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -4,7 +4,14 @@ import './Card.css';
 export const Card = ({img,title}) => {
     return(
         <div classname="card-app">
-            
+                 <a className="card-link" href={props.path}>
+               <div className = "card-img">
+                  <img src = {props.img}/>
+               </div>
+               <div className="card-txt">
+                    {props.title} 
+               </div>
+            </a> 
         </div>
 
     );
