37ad40d8b1002c9093f8925e03af84b45b7163c6
rtSblnkv <artsab001@gmail.com>
2021-05-05 11:59:26 +0400

    User Schema created

---
+++ Server/models/UserSchema.js
@@ -0,0 +1,10 @@
+import {Schema} from "mongoose";
+
+const UserSchema = new Schema({
+    login:{type:String,required:true},
+    password:{type:String,required:true},
+    nickname:String  
+});
+
+const User = model("User",UserSchema);
+export {User};
