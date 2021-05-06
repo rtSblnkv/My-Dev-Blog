d8a5e5aa65bf28d62569097a5a3d9b35c97e6683
Misha <basckackov.mixail2015@yandex.ru>
2021-05-06 16:03:32 +0400

    добавлен шаблон роутов

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import {Header} from './components/Header/Header';
 import {Footer} from './components/Footer/Footer';
-
+import {Cardblog} from './components/Card-Blog/Card_blog'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 
 
 function App() {
   return ( 
+    <Router>
     <div className ="app">
         <Header/>
+          <Route path="/Card-Blog" component={Cardblog} />
         <Footer/>
     </div>
+    </Router>
   );
 }
 
