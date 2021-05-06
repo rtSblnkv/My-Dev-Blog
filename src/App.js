e338a6e35413fe9e16d05fbcb5392dcf38798906
Artur Fattakhov <arturf60@gmail.com>
2021-05-06 17:04:24 +0400

    приняты изменения

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
 import React from 'react';
 import {Header} from './components/Header/Header';
 import {Footer} from './components/Footer/Footer';
-<<<<<<< HEAD
 
-||||||| 836a6dc
-
-
-
-
-=======
 import {Main} from './pages/Main/Main'
 import {Cardblog} from './components/Card-Blog/Card_blog'
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 
 
->>>>>>> 59f5c795b2dd351f6f362bbf556b4db94881aedc
 function App() {
   return ( 
     <Router>
