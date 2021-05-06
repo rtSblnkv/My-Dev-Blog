827b3d2d9625ca46e38f4f20e5d8085a4d952c13
Misha <basckackov.mixail2015@yandex.ru>
2021-05-06 16:57:26 +0400

    роутинг основной страницы

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Header} from './components/Header/Header';
 import {Footer} from './components/Footer/Footer';
+import {Main} from './pages/Main/Main'
 import {Cardblog} from './components/Card-Blog/Card_blog'
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
@@ -11,7 +12,7 @@ function App() {
     <Router>
     <div className ="app">
         <Header/>
-          <Route path="/Card-Blog" component={Cardblog} />
+          <Route path="/" component={Main}/>
         <Footer/>
     </div>
     </Router>
