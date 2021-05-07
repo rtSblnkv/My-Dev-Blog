f4ed8608055102a62026b71cbbc224d18a36f367
rtSblnkv <artsab001@gmail.com>
2021-05-07 15:43:54 +0400

    getSectionId function created

---
+++ Server/functions/GetId.js
@@ -0,0 +1,7 @@
+import {Section} from "../models/SectionSchema"
+
+export async function getSectionId(sectionName){
+    let section = await Section.findOne({name:sectionName}).exec();
+    return section == null ? null : section['_id'];
+
+}
