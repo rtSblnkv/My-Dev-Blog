d8b7f9ce16a8ea093920cbbc4b04f90ed9bc47a0
rtSblnkv <artsab001@gmail.com>
2021-05-07 19:20:58 +0400

    like router updated (POST)

---
+++ Server/routes/likeRouter.js
@@ -0,0 +1,17 @@
+import {Router} from "express";
+import {Like} from "../models/LikeSchema";
+
+const likeRouter = Router();
+
+likeRouter.post('/:section/:id', (req,res)=>{
+    Like.create(
+        {user:req.body.user._id,
+        post:req.params.section},(err)=>{
+            if(err)
+            {
+                console.log(err.code);
+                res.status(400).json(err.code);
+            }
+            else res.status(200).json(`User ${req.body.user} liked ${req.params.section} successfully`);
+        });
+    });
