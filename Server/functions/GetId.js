a4cfc785720fbef295ea16c8b4082129c5e56f4e
rtSblnkv <artsab001@gmail.com>
2021-05-15 18:28:49 +0400

    getter fixed

--- Server/functions/GetId.js
+++ Server/functions/GetId.js
@@ -1,6 +1,6 @@
 import {Section} from "../models/SectionSchema.js"
 
 export async function getSectionId(sectionName){
-    let section = await Section.findOne({name:sectionName}).exec();
+    let section = await Section.findOne({'name':sectionName}).exec();
     return section == null ? null : section['_id'];
 }
