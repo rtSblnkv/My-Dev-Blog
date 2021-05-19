9f179f5b07619013d2114e96cee0a717193a9ef6
rtSblnkv <artsab001@gmail.com>
2021-05-19 10:35:58 +0400

    api

--- python_api/main.py
+++ python_api/main.py
@@ -1,5 +1,6 @@
 import os
 #from pymongo import MongoClient
+from typing import List
 from motor.motor_asyncio import AsyncIOMotorClient
 from fastapi import FastAPI,Body,HTTPException,status
 from fastapi.encoders import jsonable_encoder
@@ -34,10 +35,20 @@ async def insert_post(post:Post = Body(...)):
     created_post = await db["posts"].find_one({"_id":new_post.inserted_id})
     return JSONResponse(status_code=status.HTTP_201_CREATED,content=created_post)
 
+@app.get("/users_get",response_description="Getting all users",response_model=List[User])
+async def get_users():
+    users = await db['users'].find().to_list(100)
+    return users
+
+@app.get("/posts_get",response_description="Getting all posts",response_model=List[Post])
+async def get_posts():
+    posts = await db['posts'].find().to_list(100)
+    return posts
+
+
 @app.get('/developers')
 async def get_dev():
     return [{
-        "responseType":"Data",
         "Frontend":[
             "https://vk.com/b4sk4kov",
             "https://vk.com/aaaptyppp"
